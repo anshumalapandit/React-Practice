7194b5ed8e313260af6d6a6e3324b95088387cb9
anshumalapandit <152486309+anshumalapandit@users.noreply.github.com>
2024-12-18 23:41:09 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from 'react-dom/client';
+
+const MyFirstLine=<h1>This is my first line i am so happy</h1> // this is not a html markup its is jsx which looks like html
+//now display this first line ur
+ReactDOM.createRoot(document.getElementById("root")).render(MyFirstLine);
